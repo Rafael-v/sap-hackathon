0f788e7a486882a7a6c500e3cc466bec2efdcd36
Rafael Vales <rafaelvalesb@gmail.com>
2018-08-26 08:54:34 -0300

    Update Hackathon.java

--- hackathon/Hackathon.java
+++ hackathon/Hackathon.java
@@ -1,9 +1,14 @@
 package hackathon;
 
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -15,7 +20,6 @@ import javafx.geometry.Pos;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -31,22 +35,33 @@ public class Hackathon extends Application {
     
     ListView<String> list;
     ArrayList<Produto> pdt;
+    Label label1, label2, label3;
+    String dia, mes, ano;
+    int id;
     
     public Hackathon() {
         list = new ListView<String>();
         pdt = new ArrayList();
+        label1 = new Label();
+        label2 = new Label();
+        label3 = new Label();
+        
+        pdt.add(new Produto("Abacaxi", 1, 2, "asda"));
+        pdt.add(new Produto("Maca", 1, 0, "asdfasa"));
+        pdt.add(new Produto("Pera", 1, (float)0, "asfsda"));
+        pdt.add(new Produto("Abacate", 1, (float)0, "afasfassda"));
     }
 
     @Override
     public void start(Stage stage) {
         BorderPane borderPane = new BorderPane();
         Menu menu = new Menu();
-        
-        File file = new File("C:\\Users\\Dênes\\Desktop\\sap-hackathon-master\\hackathon\\produto.json");
-        
+        System.out.println("cuzao");
+        File file = new File("C:\\Users\\rafae\\Documents\\NetBeansProjects\\hackathon\\src\\hackathon\\produto.json");
+        System.out.println("cuzao");
         menu.loadfile(stage, pdt, file);
-        File file2 = new File("C:\\Users\\Dênes\\Desktop\\sap-hackathon-master\\hackathon\\remessa.json");
-
+        System.out.println("cuzao");
+        File file2 = new File("C:\\Users\\rafae\\Documents\\NetBeansProjects\\hackathon\\src\\hackathon\\remessa.json");
         menu.loadfile(stage, pdt, file2);
         
         borderPane.setTop(getTop());
@@ -81,36 +96,36 @@ public class Hackathon extends Application {
         
         HBox hb = new HBox();
         
-        Label produto = new Label("PRODUTO: " + "Abacaxi");
-        produto.setStyle("-fx-font: 26px \"Georgia\"; -fx-text-fill: black; -fx-padding: 30 50;");
+        label1 = new Label("PRODUTO: " + "-");
+        label1.setStyle("-fx-font: 26px \"Georgia\"; -fx-text-fill: black; -fx-padding: 30 50;");
        
         Image img = new Image("File:src/imagem.png", true);
         ImageView imageView = new ImageView(img);
         imageView.setFitHeight(90);
         imageView.setFitWidth(90);
         
-        hb.getChildren().addAll(imageView, produto);
+        hb.getChildren().addAll(imageView, label1);
         
         Label msg1 = new Label("Quantia estimada para compra:");
         msg1.setStyle("-fx-font: 18px \"Georgia\"; -fx-text-fill: black");
-        Label msg2 = new Label("1234");
-        msg2.setStyle("-fx-font: 24px \"Georgia\"; -fx-text-fill: black");
+        label2 = new Label("-");
+        label2.setStyle("-fx-font: 24px \"Georgia\"; -fx-text-fill: black");
         
         String str = "nada";
-        Label msg3 = new Label("OBSERVAÇÕES: " + str);
-        msg3.setStyle("-fx-font: 16px \"Georgia\"; -fx-text-fill: black; -fx-padding: 20px");
+        label3 = new Label("OBSERVAÇÕES: " + str);
+        label3.setStyle("-fx-font: 16px \"Georgia\"; -fx-text-fill: black; -fx-padding: 20px");
         
         HBox p = new HBox();
         p.setSpacing(10);
         p.setAlignment(Pos.CENTER);
-        p.getChildren().addAll(msg1, msg2);
+        p.getChildren().addAll(msg1, label2);
         p.setStyle("-fx-padding: 40.0;");
         
         
         VBox botoes = new VBox();
         botoes.setSpacing(10);
         botoes.setAlignment(Pos.CENTER);
-        botoes.getChildren().addAll(hb, p, msg3, new Separator(), getGrafico());
+        botoes.getChildren().addAll(hb, p, label3, new Separator());
         botoes.setStyle("-fx-padding: 40.0;");
         
         
@@ -119,69 +134,27 @@ public class Hackathon extends Application {
         return pane;
     }
     
-    private LineChart getGrafico() {
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-         xAxis.setLabel("");
-        final LineChart<String,Number> lineChart = 
-                new LineChart<String,Number>(xAxis,yAxis);
-       
-        //lineChart.setTitle("Stock Monitoring, 2010");
-                          
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Portfolio 1");
+    private void updateGrafico() {
+        System.out.println(id);
+        label1.setText("PRODUTO: " + pdt.get(id).getNome());
         
-        series1.getData().add(new XYChart.Data("Jan", 23));
-        series1.getData().add(new XYChart.Data("Feb", 14));
-        series1.getData().add(new XYChart.Data("Mar", 15));
-        series1.getData().add(new XYChart.Data("Apr", 24));
-        series1.getData().add(new XYChart.Data("May", 34));
-        series1.getData().add(new XYChart.Data("Jun", 36));
-        series1.getData().add(new XYChart.Data("Jul", 22));
-        series1.getData().add(new XYChart.Data("Aug", 45));
-        series1.getData().add(new XYChart.Data("Sep", 43));
-        series1.getData().add(new XYChart.Data("Oct", 17));
-        series1.getData().add(new XYChart.Data("Nov", 29));
-        series1.getData().add(new XYChart.Data("Dec", 25));
-        
-        XYChart.Series series2 = new XYChart.Series();
-        series2.setName("Portfolio 2");
-        series2.getData().add(new XYChart.Data("Jan", 33));
-        series2.getData().add(new XYChart.Data("Feb", 34));
-        series2.getData().add(new XYChart.Data("Mar", 25));
-        series2.getData().add(new XYChart.Data("Apr", 44));
-        series2.getData().add(new XYChart.Data("May", 39));
-        series2.getData().add(new XYChart.Data("Jun", 16));
-        series2.getData().add(new XYChart.Data("Jul", 55));
-        series2.getData().add(new XYChart.Data("Aug", 54));
-        series2.getData().add(new XYChart.Data("Sep", 48));
-        series2.getData().add(new XYChart.Data("Oct", 27));
-        series2.getData().add(new XYChart.Data("Nov", 37));
-        series2.getData().add(new XYChart.Data("Dec", 29));
-        
-        XYChart.Series series3 = new XYChart.Series();
-        series3.setName("Portfolio 3");
-        series3.getData().add(new XYChart.Data("Jan", 44));
-        series3.getData().add(new XYChart.Data("Feb", 35));
-        series3.getData().add(new XYChart.Data("Mar", 36));
-        series3.getData().add(new XYChart.Data("Apr", 33));
-        series3.getData().add(new XYChart.Data("May", 31));
-        series3.getData().add(new XYChart.Data("Jun", 26));
-        series3.getData().add(new XYChart.Data("Jul", 22));
-        series3.getData().add(new XYChart.Data("Aug", 25));
-        series3.getData().add(new XYChart.Data("Sep", 43));
-        series3.getData().add(new XYChart.Data("Oct", 44));
-        series3.getData().add(new XYChart.Data("Nov", 45));
-        series3.getData().add(new XYChart.Data("Dec", 44));
-        
-        lineChart.getData().addAll(series1, series2, series3);
-        
-        lineChart.setPrefWidth(400);
-        lineChart.setPrefHeight(200);
-        
-        lineChart.setStyle("-fx-margin-bottom: 140.0;");
-        
-        return lineChart;
+        System.out.println(String.valueOf(getSoma()));
+        label2.setText(String.valueOf(getSoma()));
+    }
+    
+    private double getSoma() {
+        Calendar cal;
+        SimpleDateFormat sd = new SimpleDateFormat("MM-dd-yyyy 00:00:00");
+        try {
+            Date date = sd.parse(mes + '-' + dia + '-' + ano + " 00:00:00");
+            cal = Calendar.getInstance();
+            cal.setTime(date);
+            estatistica est = new estatistica();
+            return est.calcula_proc(pdt.get(id).Busca_Dados_Dia(cal));
+        } catch (ParseException ex) {
+            Logger.getLogger(Menu.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return 0;
     }
     
     private Pane getLeft() {
@@ -202,27 +175,30 @@ public class Hackathon extends Application {
         Label labelData = new Label("Data para busca:");
         labelData.setStyle("-fx-font: 16px \"Georgia\"; -fx-text-fill: black");
         
-        TextField dia = new TextField();
-        dia.setPromptText("Dia");
-        dia.setMaxWidth(38);
-        TextField mes = new TextField();
-        mes.setPromptText("Mês");
-        mes.setMaxWidth(38);
-        TextField ano = new TextField();
-        ano.setPromptText("Ano");
-        ano.setMaxWidth(55);
+        TextField diaa = new TextField();
+        diaa.setPromptText("Dia");
+        diaa.setMaxWidth(38);
+        TextField mess = new TextField();
+        mess.setPromptText("Mês");
+        mess.setMaxWidth(38);
+        TextField anoo = new TextField();
+        anoo.setPromptText("Ano");
+        anoo.setMaxWidth(55);
         
         Button enviar = new Button("Enviar");
         enviar.setStyle("-fx-font: 14px Georgia; -fx-padding: 7 53px;");
         enviar.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent e) {
-                System.out.println("Data: " + dia.getText() + ' ' + mes.getText() + ' ' + ano.getText());
-                System.out.println("Selecionado: " + list.getFocusModel().getFocusedIndex());
+                dia = diaa.getText();
+                mes = mess.getText();
+                ano = anoo.getText();
+                id = list.getFocusModel().getFocusedIndex();
+                updateGrafico();
             }
         });
         
         HBox data = getHBox();
-        data.getChildren().addAll(dia, mes, ano);
+        data.getChildren().addAll(diaa, mess, anoo);
         
         VBox vb = getVBox();
         vb.getChildren().addAll(labelData, data, enviar, list);
