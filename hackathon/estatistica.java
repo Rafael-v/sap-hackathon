ffbe1c6d3ac71e9289166cd13bdec5cf4164da8b
GusFX <37117188+GusFX@users.noreply.github.com>
2018-08-26 04:56:43 -0300

    Create estatistica.java

---
+++ hackathon/estatistica.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import redutordeperdas.Produto.Remessa;
+
+public class estatistica {
+
+    public static double sum(ArrayList l){
+        double ac = 0;
+        for(Object i : l){
+            ac += (double) i;
+        }
+        return ac;
+    }
+    
+    public static double med(ArrayList l){
+        return sum(l)/l.size();
+    }
+    
+    public double calcula_proc(List<Remessa> r){
+        ArrayList<Double> tot = new ArrayList();
+        
+        for(Remessa i : r){
+            double ent = i.Qtd_Entrada;
+            double perd = i.Qtd_Perda;
+            tot.add(ent-perd);
+        }
+        
+        return med(tot);
+    }
+}
